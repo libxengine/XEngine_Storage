dab25d30b8dc534f941e33732b78e94d6ef2e2ad
qyt <486179@qq.com>
2021-08-13 14:13:00 +0800

    added:auto speed and download try number for configure

--- XEngine_Source/StorageModule_Config/Config_Define.h
+++ XEngine_Source/StorageModule_Config/Config_Define.h
@@ -84,6 +84,8 @@ typedef struct tag_XEngine_ServerConfig
     }st_XProxy;
 	struct
 	{
+        BOOL bAutoSpeed;
+        int nDLTry;
         __int64x nMaxUPLoader;
         __int64x nMaxDNLoader;
 	}st_XLimit;
--- XEngine_Source/StorageModule_Config/Config_Json/Config_Json.cpp
+++ XEngine_Source/StorageModule_Config/Config_Json/Config_Json.cpp
@@ -155,13 +155,15 @@ BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG 
     _tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyPass.tszDLPass, st_JsonXProxyPass["tszDLPass"].asCString());
     _tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyPass.tszUPPass, st_JsonXProxyPass["tszUPPass"].asCString());
 
-	if (st_JsonRoot["XLimit"].empty() || (2 != st_JsonRoot["XLimit"].size()))
+	if (st_JsonRoot["XLimit"].empty() || (4 != st_JsonRoot["XLimit"].size()))
 	{
 		Config_IsErrorOccur = TRUE;
 		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XSTORAGE;
 		return FALSE;
 	}
 	Json::Value st_JsonXLimit = st_JsonRoot["XLimit"];
+    pSt_ServerConfig->st_XLimit.bAutoSpeed = st_JsonXLimit["bAutoSpeed"].asUInt();
+    pSt_ServerConfig->st_XLimit.nDLTry = st_JsonXLimit["nDLTry"].asUInt();
 	pSt_ServerConfig->st_XLimit.nMaxDNLoader = st_JsonXLimit["nMaxDNLoad"].asInt64();
     pSt_ServerConfig->st_XLimit.nMaxUPLoader = st_JsonXLimit["nMaxUPLoad"].asInt64();
 
