3906c67a13246b01bb851b1d9010c388e99fe363
qyt <486179@qq.com>
2021-12-10 10:09:05 +0800

    configure module:added bUPHash configure

--- XEngine_Source/StorageModule_Config/Config_Json/Config_Json.cpp
+++ XEngine_Source/StorageModule_Config/Config_Json/Config_Json.cpp
@@ -22,119 +22,138 @@ CConfig_Json::~CConfig_Json()
 //////////////////////////////////////////////////////////////////////////
 //                        公用函数
 //////////////////////////////////////////////////////////////////////////
-BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG *pSt_ServerConfig)
+/********************************************************************
+函数名称：Config_Json_File
+函数功能：读取基本配置文件
+ 参数.一：lpszConfigFile
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入配置文件
+ 参数.二：pSt_ServerConfig
+  In/Out：Out
+  类型：数据结构指针
+  可空：N
+  意思：输出读取到的信息
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile, XENGINE_SERVERCONFIG* pSt_ServerConfig)
 {
-    Config_IsErrorOccur = FALSE;
+	Config_IsErrorOccur = FALSE;
 
-    if ((NULL == lpszConfigFile) || (NULL == pSt_ServerConfig))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_PARAMENT;
-        return FALSE;
-    }
-    JSONCPP_STRING st_JsonError;
-    Json::Value st_JsonRoot;
-    Json::CharReaderBuilder st_JsonBuilder;
+	if ((NULL == lpszConfigFile) || (NULL == pSt_ServerConfig))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_PARAMENT;
+		return FALSE;
+	}
+	JSONCPP_STRING st_JsonError;
+	Json::Value st_JsonRoot;
+	Json::CharReaderBuilder st_JsonBuilder;
 
-    FILE* pSt_File = _tfopen(lpszConfigFile, _T("rb"));
-    if (NULL == pSt_File)
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_PARAMENT;
-        return FALSE;
-    }
-    int nCount = 0;
-    TCHAR tszMsgBuffer[4096];
-    while (1)
-    {
-        int nRet = fread(tszMsgBuffer + nCount, 1, 2048, pSt_File);
-        if (nRet <= 0)
-        {
-            break;
-        }
-        nCount += nRet;
-    }
-    fclose(pSt_File);
+	FILE* pSt_File = _tfopen(lpszConfigFile, _T("rb"));
+	if (NULL == pSt_File)
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_PARAMENT;
+		return FALSE;
+	}
+	int nCount = 0;
+	TCHAR tszMsgBuffer[4096];
+	while (1)
+	{
+		int nRet = fread(tszMsgBuffer + nCount, 1, 2048, pSt_File);
+		if (nRet <= 0)
+		{
+			break;
+		}
+		nCount += nRet;
+	}
+	fclose(pSt_File);
 
-    std::unique_ptr<Json::CharReader> const pSt_JsonReader(st_JsonBuilder.newCharReader());
-    if (!pSt_JsonReader->parse(tszMsgBuffer, tszMsgBuffer + nCount, &st_JsonRoot, &st_JsonError))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_PARSE;
-        return FALSE;
-    }
-    _tcscpy(pSt_ServerConfig->tszIPAddr,st_JsonRoot["tszIPAddr"].asCString());
-    pSt_ServerConfig->bDeamon = st_JsonRoot["bDeamon"].asInt();
-    pSt_ServerConfig->nCenterPort = st_JsonRoot["nCenterPort"].asInt();
-    pSt_ServerConfig->nStorageDLPort = st_JsonRoot["nStorageDLPort"].asInt();
+	std::unique_ptr<Json::CharReader> const pSt_JsonReader(st_JsonBuilder.newCharReader());
+	if (!pSt_JsonReader->parse(tszMsgBuffer, tszMsgBuffer + nCount, &st_JsonRoot, &st_JsonError))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_PARSE;
+		return FALSE;
+	}
+	_tcscpy(pSt_ServerConfig->tszIPAddr, st_JsonRoot["tszIPAddr"].asCString());
+	pSt_ServerConfig->bDeamon = st_JsonRoot["bDeamon"].asInt();
+	pSt_ServerConfig->nCenterPort = st_JsonRoot["nCenterPort"].asInt();
+	pSt_ServerConfig->nStorageDLPort = st_JsonRoot["nStorageDLPort"].asInt();
 	pSt_ServerConfig->nStorageUPPort = st_JsonRoot["nStorageUPPort"].asInt();
 	pSt_ServerConfig->nP2XPPort = st_JsonRoot["nP2XPPort"].asInt();
 
-    if (st_JsonRoot["XMax"].empty() || (7 != st_JsonRoot["XMax"].size()))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XMAX;
-        return FALSE;
-    }
-    Json::Value st_JsonXMax = st_JsonRoot["XMax"];
-    pSt_ServerConfig->st_XMax.nMaxClient = st_JsonXMax["MaxClient"].asInt();
-    pSt_ServerConfig->st_XMax.nMaxQueue = st_JsonXMax["MaxQueue"].asInt();
-    pSt_ServerConfig->st_XMax.nIOThread = st_JsonXMax["IOThread"].asInt();
-    pSt_ServerConfig->st_XMax.nCenterThread = st_JsonXMax["CenterThread"].asInt();
-    pSt_ServerConfig->st_XMax.nStorageUPThread = st_JsonXMax["nStorageUPThread"].asInt();
-    pSt_ServerConfig->st_XMax.nStorageDLThread = st_JsonXMax["nStorageDLThread"].asInt();
-    pSt_ServerConfig->st_XMax.nP2XPThread = st_JsonXMax["nP2XPThread"].asInt();
+	if (st_JsonRoot["XMax"].empty() || (7 != st_JsonRoot["XMax"].size()))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XMAX;
+		return FALSE;
+	}
+	Json::Value st_JsonXMax = st_JsonRoot["XMax"];
+	pSt_ServerConfig->st_XMax.nMaxClient = st_JsonXMax["MaxClient"].asInt();
+	pSt_ServerConfig->st_XMax.nMaxQueue = st_JsonXMax["MaxQueue"].asInt();
+	pSt_ServerConfig->st_XMax.nIOThread = st_JsonXMax["IOThread"].asInt();
+	pSt_ServerConfig->st_XMax.nCenterThread = st_JsonXMax["CenterThread"].asInt();
+	pSt_ServerConfig->st_XMax.nStorageUPThread = st_JsonXMax["nStorageUPThread"].asInt();
+	pSt_ServerConfig->st_XMax.nStorageDLThread = st_JsonXMax["nStorageDLThread"].asInt();
+	pSt_ServerConfig->st_XMax.nP2XPThread = st_JsonXMax["nP2XPThread"].asInt();
 
-    if (st_JsonRoot["XTime"].empty() || (6 != st_JsonRoot["XTime"].size()))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XTIME;
-        return FALSE;
-    }
-    Json::Value st_JsonXTime = st_JsonRoot["XTime"];
-    pSt_ServerConfig->st_XTime.bHBTime = st_JsonXTime["bHBTime"].asInt();
-    pSt_ServerConfig->st_XTime.nDBMonth = st_JsonXTime["nDBMonth"].asInt();
-    pSt_ServerConfig->st_XTime.nTimeCheck = st_JsonXTime["nTimeCheck"].asInt();
-    pSt_ServerConfig->st_XTime.nCenterTimeOut = st_JsonXTime["nCenterTimeOut"].asInt();
-    pSt_ServerConfig->st_XTime.nStorageTimeOut = st_JsonXTime["nStorageTimeOut"].asInt();
-    pSt_ServerConfig->st_XTime.nP2XPTimeOut = st_JsonXTime["nP2XPTimeOut"].asInt();
+	if (st_JsonRoot["XTime"].empty() || (6 != st_JsonRoot["XTime"].size()))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XTIME;
+		return FALSE;
+	}
+	Json::Value st_JsonXTime = st_JsonRoot["XTime"];
+	pSt_ServerConfig->st_XTime.bHBTime = st_JsonXTime["bHBTime"].asInt();
+	pSt_ServerConfig->st_XTime.nDBMonth = st_JsonXTime["nDBMonth"].asInt();
+	pSt_ServerConfig->st_XTime.nTimeCheck = st_JsonXTime["nTimeCheck"].asInt();
+	pSt_ServerConfig->st_XTime.nCenterTimeOut = st_JsonXTime["nCenterTimeOut"].asInt();
+	pSt_ServerConfig->st_XTime.nStorageTimeOut = st_JsonXTime["nStorageTimeOut"].asInt();
+	pSt_ServerConfig->st_XTime.nP2XPTimeOut = st_JsonXTime["nP2XPTimeOut"].asInt();
 
-    if (st_JsonRoot["XLog"].empty() || (3 != st_JsonRoot["XLog"].size()))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XLOG;
-        return FALSE;
-    }
-    Json::Value st_JsonXLog = st_JsonRoot["XLog"];
-    pSt_ServerConfig->st_XLog.nMaxSize = st_JsonXLog["MaxSize"].asInt();
-    pSt_ServerConfig->st_XLog.nMaxCount = st_JsonXLog["MaxCount"].asInt();
-    pSt_ServerConfig->st_XLog.nLogLeave = st_JsonXLog["LogLeave"].asInt();
+	if (st_JsonRoot["XLog"].empty() || (3 != st_JsonRoot["XLog"].size()))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XLOG;
+		return FALSE;
+	}
+	Json::Value st_JsonXLog = st_JsonRoot["XLog"];
+	pSt_ServerConfig->st_XLog.nMaxSize = st_JsonXLog["MaxSize"].asInt();
+	pSt_ServerConfig->st_XLog.nMaxCount = st_JsonXLog["MaxCount"].asInt();
+	pSt_ServerConfig->st_XLog.nLogLeave = st_JsonXLog["LogLeave"].asInt();
 
-    if (st_JsonRoot["XSql"].empty() || (6 != st_JsonRoot["XSql"].size()))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XSQL;
-        return FALSE;
-    }
-    Json::Value st_JsonXSql = st_JsonRoot["XSql"];
+	if (st_JsonRoot["XSql"].empty() || (6 != st_JsonRoot["XSql"].size()))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XSQL;
+		return FALSE;
+	}
+	Json::Value st_JsonXSql = st_JsonRoot["XSql"];
 
-    pSt_ServerConfig->st_XSql.nSQLType = st_JsonXSql["SQLType"].asInt();
-    pSt_ServerConfig->st_XSql.nSQLPort = st_JsonXSql["SQLPort"].asInt();
+	pSt_ServerConfig->st_XSql.nSQLType = st_JsonXSql["SQLType"].asInt();
+	pSt_ServerConfig->st_XSql.nSQLPort = st_JsonXSql["SQLPort"].asInt();
 	_tcscpy(pSt_ServerConfig->st_XSql.tszSQLAddr, st_JsonXSql["SQLAddr"].asCString());
 	_tcscpy(pSt_ServerConfig->st_XSql.tszSQLUser, st_JsonXSql["SQLUser"].asCString());
 	_tcscpy(pSt_ServerConfig->st_XSql.tszSQLPass, st_JsonXSql["SQLPass"].asCString());
 	_tcscpy(pSt_ServerConfig->st_XSql.tszSQLFile, st_JsonXSql["SQLFile"].asCString());
 
-    if (st_JsonRoot["XStorage"].empty() || (3 != st_JsonRoot["XStorage"].size()))
-    {
-        Config_IsErrorOccur = TRUE;
-        Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XSTORAGE;
-        return FALSE;
-    }
-    Json::Value st_JsonXStorage = st_JsonRoot["XStorage"];
-    pSt_ServerConfig->st_XStorage.nHashMode = st_JsonXStorage["nHashMode"].asInt();
-    pSt_ServerConfig->st_XStorage.bResumable = st_JsonXStorage["bResumable"].asInt();
-    _tcscpy(pSt_ServerConfig->st_XStorage.tszFileDir, st_JsonXStorage["tszFileDir"].asCString());
+	if (st_JsonRoot["XStorage"].empty() || (4 != st_JsonRoot["XStorage"].size()))
+	{
+		Config_IsErrorOccur = TRUE;
+		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XSTORAGE;
+		return FALSE;
+	}
+	Json::Value st_JsonXStorage = st_JsonRoot["XStorage"];
+	pSt_ServerConfig->st_XStorage.nHashMode = st_JsonXStorage["nHashMode"].asInt();
+	pSt_ServerConfig->st_XStorage.bUPHash = st_JsonXStorage["bUPHash"].asInt();
+	pSt_ServerConfig->st_XStorage.bResumable = st_JsonXStorage["bResumable"].asInt();
+	_tcscpy(pSt_ServerConfig->st_XStorage.tszFileDir, st_JsonXStorage["tszFileDir"].asCString());
 
 	if (st_JsonRoot["XProxy"].empty() || (2 != st_JsonRoot["XProxy"].size()))
 	{
@@ -142,18 +161,18 @@ BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG 
 		Config_dwErrorCode = ERROR_XENGINE_BLOGIC_CONFIG_JSON_XPROXY;
 		return FALSE;
 	}
-    Json::Value st_JsonXProxy = st_JsonRoot["XProxy"];
-    Json::Value st_JsonXProxyAuth = st_JsonXProxy["XProxyAuth"];
-    Json::Value st_JsonXProxyPass = st_JsonXProxy["XProxyPass"];
+	Json::Value st_JsonXProxy = st_JsonRoot["XProxy"];
+	Json::Value st_JsonXProxyAuth = st_JsonXProxy["XProxyAuth"];
+	Json::Value st_JsonXProxyPass = st_JsonXProxy["XProxyPass"];
 
-    pSt_ServerConfig->st_XProxy.st_XProxyAuth.bAuth = st_JsonXProxyAuth["bAuth"].asInt();
-    _tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyAuth.tszAuthProxy, st_JsonXProxyAuth["tszAuthProxy"].asCString());
-    _tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyAuth.tszUserList, st_JsonXProxyAuth["tszUserList"].asCString());
+	pSt_ServerConfig->st_XProxy.st_XProxyAuth.bAuth = st_JsonXProxyAuth["bAuth"].asInt();
+	_tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyAuth.tszAuthProxy, st_JsonXProxyAuth["tszAuthProxy"].asCString());
+	_tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyAuth.tszUserList, st_JsonXProxyAuth["tszUserList"].asCString());
 
-    pSt_ServerConfig->st_XProxy.st_XProxyPass.bDLPass = st_JsonXProxyPass["bDLGet"].asInt();
-    pSt_ServerConfig->st_XProxy.st_XProxyPass.bUPPass = st_JsonXProxyPass["bUPGet"].asInt();
-    _tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyPass.tszDLPass, st_JsonXProxyPass["tszDLPass"].asCString());
-    _tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyPass.tszUPPass, st_JsonXProxyPass["tszUPPass"].asCString());
+	pSt_ServerConfig->st_XProxy.st_XProxyPass.bDLPass = st_JsonXProxyPass["bDLGet"].asInt();
+	pSt_ServerConfig->st_XProxy.st_XProxyPass.bUPPass = st_JsonXProxyPass["bUPGet"].asInt();
+	_tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyPass.tszDLPass, st_JsonXProxyPass["tszDLPass"].asCString());
+	_tcscpy(pSt_ServerConfig->st_XProxy.st_XProxyPass.tszUPPass, st_JsonXProxyPass["tszUPPass"].asCString());
 
 	if (st_JsonRoot["XLimit"].empty() || (3 != st_JsonRoot["XLimit"].size()))
 	{
@@ -162,9 +181,9 @@ BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG 
 		return FALSE;
 	}
 	Json::Value st_JsonXLimit = st_JsonRoot["XLimit"];
-    pSt_ServerConfig->st_XLimit.nDLTry = st_JsonXLimit["nDLTry"].asUInt();
+	pSt_ServerConfig->st_XLimit.nDLTry = st_JsonXLimit["nDLTry"].asUInt();
 	pSt_ServerConfig->st_XLimit.nMaxDNLoader = st_JsonXLimit["nMaxDNLoad"].asInt64();
-    pSt_ServerConfig->st_XLimit.nMaxUPLoader = st_JsonXLimit["nMaxUPLoad"].asInt64();
+	pSt_ServerConfig->st_XLimit.nMaxUPLoader = st_JsonXLimit["nMaxUPLoad"].asInt64();
 
 	if (st_JsonRoot["XP2xp"].empty() || (5 != st_JsonRoot["XP2xp"].size()))
 	{
@@ -173,11 +192,11 @@ BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG 
 		return FALSE;
 	}
 	Json::Value st_JsonP2xp = st_JsonRoot["XP2xp"];
-    pSt_ServerConfig->st_P2xp.nMode = st_JsonP2xp["nMode"].asInt();
-    pSt_ServerConfig->st_P2xp.nTime = st_JsonP2xp["nTime"].asInt();
-    pSt_ServerConfig->st_P2xp.nRVPort = st_JsonP2xp["nRVPort"].asInt();
-    pSt_ServerConfig->st_P2xp.nSDPort = st_JsonP2xp["nSDPort"].asInt();
-    _tcscpy(pSt_ServerConfig->st_P2xp.tszQQWryFile, st_JsonP2xp["tszQQWryFile"].asCString());
+	pSt_ServerConfig->st_P2xp.nMode = st_JsonP2xp["nMode"].asInt();
+	pSt_ServerConfig->st_P2xp.nTime = st_JsonP2xp["nTime"].asInt();
+	pSt_ServerConfig->st_P2xp.nRVPort = st_JsonP2xp["nRVPort"].asInt();
+	pSt_ServerConfig->st_P2xp.nSDPort = st_JsonP2xp["nSDPort"].asInt();
+	_tcscpy(pSt_ServerConfig->st_P2xp.tszQQWryFile, st_JsonP2xp["tszQQWryFile"].asCString());
 
 	if (st_JsonRoot["XCert"].empty() || (6 != st_JsonRoot["XCert"].size()))
 	{
@@ -187,16 +206,16 @@ BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG 
 	}
 	Json::Value st_JsonCert = st_JsonRoot["XCert"];
 	pSt_ServerConfig->st_XCert.bDLEnable = st_JsonCert["bDLEnable"].asInt();
-    pSt_ServerConfig->st_XCert.bUPEnable = st_JsonCert["bUPEnable"].asInt();
-    pSt_ServerConfig->st_XCert.bCHEnable = st_JsonCert["bCHEnable"].asInt();
-    pSt_ServerConfig->st_XCert.nSslType = st_JsonCert["nSslType"].asInt();
-    if (!st_JsonP2xp["tszCertChain"].isNull())
-    {
-        _tcscpy(pSt_ServerConfig->st_XCert.tszCertChain, st_JsonP2xp["tszCertChain"].asCString());
-    }
+	pSt_ServerConfig->st_XCert.bUPEnable = st_JsonCert["bUPEnable"].asInt();
+	pSt_ServerConfig->st_XCert.bCHEnable = st_JsonCert["bCHEnable"].asInt();
+	pSt_ServerConfig->st_XCert.nSslType = st_JsonCert["nSslType"].asInt();
+	if (!st_JsonP2xp["tszCertChain"].isNull())
+	{
+		_tcscpy(pSt_ServerConfig->st_XCert.tszCertChain, st_JsonP2xp["tszCertChain"].asCString());
+	}
 	if (!st_JsonP2xp["tszCertKey"].isNull())
 	{
-        _tcscpy(pSt_ServerConfig->st_XCert.tszCertKey, st_JsonP2xp["tszCertKey"].asCString());
+		_tcscpy(pSt_ServerConfig->st_XCert.tszCertKey, st_JsonP2xp["tszCertKey"].asCString());
 	}
 
 	if (st_JsonRoot["XVer"].empty() || (1 != st_JsonRoot["XVer"].size()))
@@ -207,13 +226,31 @@ BOOL CConfig_Json::Config_Json_File(LPCTSTR lpszConfigFile,XENGINE_SERVERCONFIG 
 	}
 	Json::Value st_JsonXVer = st_JsonRoot["XVer"];
 
-    pSt_ServerConfig->st_XVer.pStl_ListStorage = new list<string>;
-    for (unsigned int i = 0; i < st_JsonXVer["StorageVersion"].size(); i++)
-    {
-        pSt_ServerConfig->st_XVer.pStl_ListStorage->push_back(st_JsonXVer["StorageVersion"][i].asCString());
-    }
-    return TRUE;
+	pSt_ServerConfig->st_XVer.pStl_ListStorage = new list<string>;
+	for (unsigned int i = 0; i < st_JsonXVer["StorageVersion"].size(); i++)
+	{
+		pSt_ServerConfig->st_XVer.pStl_ListStorage->push_back(st_JsonXVer["StorageVersion"][i].asCString());
+	}
+	return TRUE;
 }
+/********************************************************************
+函数名称：Config_Json_LoadBalance
+函数功能：读取负载均衡配置文件
+ 参数.一：lpszConfigFile
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入配置文件
+ 参数.二：pSt_ServerConfig
+  In/Out：Out
+  类型：数据结构指针
+  可空：N
+  意思：输出读取到的信息
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
 BOOL CConfig_Json::Config_Json_LoadBalance(LPCTSTR lpszConfigFile, XENGINE_LBCONFIG* pSt_ServerConfig)
 {
 	Config_IsErrorOccur = FALSE;
