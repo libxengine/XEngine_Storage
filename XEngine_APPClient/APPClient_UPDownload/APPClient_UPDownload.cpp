711a5854b36fe78842ba29ddea57cac3a38002a4
qyt <486179@qq.com>
2022-07-27 09:48:10 +0800

    update:updownload and download example

--- XEngine_APPClient/APPClient_UPDownload/APPClient_UPDownload.cpp
+++ XEngine_APPClient/APPClient_UPDownload/APPClient_UPDownload.cpp
@@ -5,12 +5,14 @@
 #pragma comment(lib,"XEngine_Core/XEngine_OPenSsl")
 #pragma comment(lib,"XEngine_NetHelp/NetHelp_APIHelp")
 #pragma comment(lib,"Ws2_32")
+#pragma comment(lib,"../../XEngine_Source/Debug/jsoncpp")
 #else
 #endif
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <list>
+#include <json/json.h>
 #include <XEngine_Include/XEngine_CommHdr.h>
 #include <XEngine_Include/XEngine_Types.h>
 #include <XEngine_Include/XEngine_ProtocolHdr.h>
@@ -24,8 +26,8 @@ using namespace std;
 
 //需要优先配置XEngine
 //WINDOWS使用VS2022 x86 debug 编译
-//linux使用下面的命令编译
-//g++ -std=c++17 -Wall -g APPClient_UPDownload.cpp -o APPClient_UPDownload.exe -L /usr/local/lib/XEngine_Release/XEngine_BaseLib -L /usr/local/lib/XEngine_Release/XEngine_Core -L /usr/local/lib/XEngine_Release/XEngine_NetHelp -lXEngine_BaseLib -lXEngine_OPenSsl -lNetHelp_APIHelp
+//linux::g++ -std=c++17 -Wall -g APPClient_UPDownload.cpp -o APPClient_UPDownload.exe -I ../../XEngine_Source/XEngine_ThirdPart/jsoncpp -L /usr/local/lib/XEngine_Release/XEngine_BaseLib -L /usr/local/lib/XEngine_Release/XEngine_Core -L /usr/local/lib/XEngine_Release/XEngine_NetHelp -L ../../XEngine_Source/XEngine_ThirdPart/jsoncpp -lXEngine_BaseLib -lXEngine_OPenSsl -lNetHelp_APIHelp -ljsoncpp
+//macos::g++ -std=c++17 -Wall -g APPClient_UPDownload.cpp -o APPClient_UPDownload.exe -I ../../XEngine_Source/XEngine_ThirdPart/jsoncpp -L ../../XEngine_Source/XEngine_ThirdPart/jsoncpp -lXEngine_BaseLib -lXEngine_OPenSsl -lNetHelp_APIHelp -ljsoncpp
 
 //上传文件
 void File_UPLoad()
@@ -36,11 +38,13 @@ void File_UPLoad()
 	TCHAR* ptszMsgBuffer = NULL;
 	TCHAR tszBaseBuffer[128];
 	TCHAR tszHdrBuffer[MAX_PATH];
+	TCHAR tszKeyBuffer[MAX_PATH];
 	LPCTSTR lpszMsgBuffer = _T("01234");
 	LPCTSTR lpszMsgBuffer2 = _T("56789");
 
 	memset(tszBaseBuffer, '\0', sizeof(tszBaseBuffer));
 	memset(tszHdrBuffer, '\0', MAX_PATH);
+	memset(tszKeyBuffer, '\0', MAX_PATH);
 	OPenSsl_Help_BasicEncoder("123123aa", "123123", tszBaseBuffer);
 
 	_stprintf(tszHdrBuffer, _T("Range: bytes=0-5/10\r\nAuthorization: %s\r\n"), tszBaseBuffer);
@@ -49,11 +53,21 @@ void File_UPLoad()
 		printf("upload failed:%lX\n", APIHelp_GetLastError());
 		return;
 	}
+	JSONCPP_STRING st_JsonError;
+	Json::Value st_JsonRoot;
+	Json::CharReaderBuilder st_JsonBuilder;
+	std::unique_ptr<Json::CharReader> const pSt_JsonReader(st_JsonBuilder.newCharReader());
+	if (!pSt_JsonReader->parse(ptszMsgBuffer, ptszMsgBuffer + nLen, &st_JsonRoot, &st_JsonError))
+	{
+		return;
+	}
+	_tcscpy(tszKeyBuffer, st_JsonRoot["lpszBuckKey"].asCString());
+
 	printf("upload:%d\n", nCode);
 	BaseLib_OperatorMemory_FreeCStyle((XPPMEM)&ptszMsgBuffer);
-
+	//断点续传必须指定storagekey
 	memset(tszHdrBuffer, '\0', MAX_PATH);
-	_stprintf(tszHdrBuffer, _T("Range: bytes=5-9/10\r\nAuthorization: %s\r\n"), tszBaseBuffer);
+	_stprintf(tszHdrBuffer, _T("Range: bytes=5-9/10\r\nAuthorization: %s\r\nStorageKey: %s\r\n"), tszBaseBuffer, tszKeyBuffer);
 	if (!APIHelp_HttpRequest_Post(lpszUrl, lpszMsgBuffer2, &nCode, &ptszMsgBuffer, &nLen, tszHdrBuffer))
 	{
 		printf("upload failed:%lX\n", APIHelp_GetLastError());
