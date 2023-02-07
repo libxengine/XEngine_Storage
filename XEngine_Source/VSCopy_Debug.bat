51a3b05a1ccfe65d20462ca7ebcdaeb0655388f2
qyt <486179@qq.com>
2023-02-07 13:53:26 +0800

    update:vscopy

--- XEngine_Source/VSCopy_Debug.bat
+++ XEngine_Source/VSCopy_Debug.bat
@@ -5,13 +5,8 @@ copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_ManagePool.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_OPenSsl.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_SystemApi.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIClient.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_DataBase.dll" "./"
-copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_Packets.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_XLog.dll" "./"
-
-copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIHelp.dll" "./"
-
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_HttpServer.dll" "./"
-
-copy /y "D:\XEngine\XEngine_SourceCode\Debug\libcurl-d.dll" "./"
--- XEngine_Source/VSCopy_x86.bat
+++ XEngine_Source/VSCopy_x86.bat
@@ -5,12 +5,10 @@ copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_ManagePool.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_OPenSsl.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_SystemSdk\XEngine_SystemApi.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_NetHelp\NetHelp_APIClient.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\HelpComponents_DataBase.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\HelpComponents_XLog.dll" "./"
-
-copy /y "%XEngine_Lib32%\XEngine_NetHelp\NetHelp_APIHelp.dll" "./"
-
 copy /y "%XEngine_Lib32%\XEngine_RfcComponents\RfcComponents_HttpServer.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\bson-1.0.dll" "./"
@@ -20,7 +18,7 @@ copy /y "%XEngine_Lib32%\XEngine_HelpComponents\libpq.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\sqlite3.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\zlib1.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\plugin\caching_sha2_password.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_NetHelp\libcurl.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_NetHelp\nghttp2.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_LibEx\libcrypto-3.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_LibEx\libssl-3.dll" "./"
-copy /y "%XEngine_Lib32%\XEngine_LibEx\libcurl.dll" "./"
-copy /y "%XEngine_Lib32%\XEngine_LibEx\nghttp2.dll" "./"
