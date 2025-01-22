28b2928c43f62965833af4afafe75f622525e179
qyt <486179@qq.com>
2025-01-22 15:49:06 +0800

    improved:get send report time

--- XEngine_Source/XEngine_StorageApp/XEngine_StorageApp.cpp
+++ XEngine_Source/XEngine_StorageApp/XEngine_StorageApp.cpp
@@ -575,10 +575,9 @@ int main(int argc, char** argv)
 	//发送信息报告
 	if (st_ServiceCfg.st_XReport.bEnable && !bIsTest)
 	{
-		if (InfoReport_APIMachine_Send(st_ServiceCfg.st_XReport.tszAPIUrl, st_ServiceCfg.st_XReport.tszServiceName))
+		__int64x nTimeCount = 0;
+		if (InfoReport_APIMachine_Send(st_ServiceCfg.st_XReport.tszAPIUrl, st_ServiceCfg.st_XReport.tszServiceName, &nTimeCount))
 		{
-			__int64x nTimeCount = 0;
-			InfoReport_APIMachine_GetTime(st_ServiceCfg.st_XReport.tszAPIUrl, st_ServiceCfg.st_XReport.tszServiceName, &nTimeCount);
 			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("启动服务中，启动信息报告给API服务器:%s 成功,报告次数:%lld"), st_ServiceCfg.st_XReport.tszAPIUrl, nTimeCount);
 		}
 		else
